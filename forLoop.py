3c9d31075aa0f8e578f4e47482b99c1139777d61
HonzaStranky <honza.hejny@email.cz>
2026-07-14 19:42:03 +0200

    mob detekce
    mob detekce 🗡️

---
+++ forLoop.py
@@ -0,0 +1,33 @@
+# =================================================================
+# VÝZVA: DETEKTOR NEBEZPEČNÝCH MOBŮ
+# =================================================================
+
+# 1. Tady máš seznam mobů, které hráč potkal na výpravě:
+mobove = ["Creeper", "Slepice", "Zombie", "Prase", "Wither", "Kráva"]
+
+# 2. Vytvoř si proměnnou pro počítání nebezpečných mobů (na začátku jich je 0)
+# (Nápověda: např. pocet_nepratel = 0)
+
+
+# 3. Napiš "for" cyklus, který projde seznam "moby":
+# (Nápověda: for mob in mobove:)
+
+    # uvnitř cyklu (odsazené na 1 Tabulátor):
+    
+    # 4. Pomocí podmínek (if / elif / else) roztřiď moby:
+    #    POZOR!!!!!!!!!! na zacatku MUSI, byt velke pismeno protoze je to tak i v listu mobove priklad "Wither", "Zombie"
+    #    A) Pokud je mob "Creeper" nebo mob je "Wither":
+    #       - Vytiskni: "[POZOR] {mob} je extrémně nebezpečný!"
+    #       - Přičti k počtu nebezpečných mobů jedničku (+1)
+    #
+    #    B) Pokud je mob "Zombie":
+    #       - Vytiskni: "[VAROVÁNÍ] {mob} je nepřítel, ale dá se zvládnout."
+    #       - Přičti k počtu nebezpečných mobů jedničku (+1)
+    #
+    #    C) Ve všech ostatních případech (ostatní mobové jsou mírumilovní):
+    #       - Vytiskni: "[KLID] {mob} je přítel."
+
+
+# 5. Úplně na konci programu (mimo cyklus, bez odsazení vlevo)
+#    vytiskni celkový počet nebezpečných mobů, které jsi potkal.
+#    (Napiš např. "Počet nepřátel v okolí: X")
