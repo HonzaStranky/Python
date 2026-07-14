0b491325771d7d4f84a4aa981eff9e0be38c0ceb
HonzaStranky <honza.hejny@email.cz>
2026-07-14 20:33:37 +0200

    forLoop2
    forLoop2

---
+++ forLoop2.py
@@ -0,0 +1,33 @@
+# =================================================================
+# VÝZVA: FILTR TRUHLICE S LOOTEM
+# =================================================================
+
+# 1. Tady máš věci, které jsi našel v truhle v dungeonu:
+truhla = ["Cobblestone", "Diamond", "Dirt", "Netherite Scrap", "Gravel", "Diamond", "Rotten Flesh"]
+
+# 2. Vytvoř si prázdný seznam (list), kam budeš ukládat jen ty cenné věci.
+# (Nápověda: např. cenne_veci = [])
+
+
+
+# 3. Napiš "for" cyklus, který projde seznam "truhla":
+# (Nápověda: for vec in truhla:)
+
+
+    # Uvnitř cyklu (nezapomeň na 1 tabulátor odsazení!):
+
+    # 4. Pomocí podmínek (if/else) zkontroluj, jestli je věc cenná:
+    #
+    #    A) Pokud se věc rovná "Diamond" NEBO se rovná "Netherite Scrap":
+    #       - Vytiskni: "Našel jsem vzácnost: {vec}!"
+    #       - Přidej tuhle věc do svého seznamu cenných věcí.
+    #         (Nápověda pro přidání do listu: cenne_veci.append(vec))
+    #
+    #    B) Jinak (u všeho ostatního bláta a kamení):
+    #       - Vytiskni: "Tohle je odpad: {vec}"
+
+
+
+# 5. Úplně na konci (mimo cyklus, bez odsazení vlevo):
+#    Vytiskni svůj nový seznam cenných věcí, ať vidíš, co sis odnesl domů.
+#    (Vypiš např. "Moje cenné věci: [seznam]")
