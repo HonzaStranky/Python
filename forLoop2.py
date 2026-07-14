403e9c6c0f13fd84dbbcc67452ac01cbe30111ef
HonzaStranky <honza.hejny@email.cz>
2026-07-14 20:41:07 +0200

    Fix indentation and comments in forLoop2.py

--- forLoop2.py
+++ forLoop2.py
@@ -14,20 +14,20 @@ truhla = ["Cobblestone", "Diamond", "Dirt", "Netherite Scrap", "Gravel", "Diamon
 # (Nápověda: for vec in truhla:)
 
 
-    # Uvnitř cyklu (nezapomeň na 1 tabulátor odsazení!):
+    # Uvnitř cyklu (nezapomeň na 1 tabulátor odsazení!):
 
-    # 4. Pomocí podmínek (if/else) zkontroluj, jestli je věc cenná:
-    #
-    #    A) Pokud se věc rovná "Diamond" NEBO se rovná "Netherite Scrap":
-    #       - Vytiskni: "Našel jsem vzácnost: {vec}!"
-    #       - Přidej tuhle věc do svého seznamu cenných věcí.
-    #         (Nápověda pro přidání do listu: cenne_veci.append(vec))
-    #
-    #    B) Jinak (u všeho ostatního bláta a kamení):
-    #       - Vytiskni: "Tohle je odpad: {vec}"
+    # 4. Pomocí podmínek (if/else) zkontroluj, jestli je věc cenná:
+    #
+    #    A) Pokud se věc rovná "Diamond" NEBO se rovná "Netherite Scrap":
+    #       - Vytiskni: "Našel jsem vzácnost: {vec}!"
+    #       - Přidej tuhle věc do svého seznamu cenných věcí.
+    #         (Nápověda pro přidání do listu: cenne_veci.append(vec))
+    #
+    #    B) Jinak (u všeho ostatního bláta a kamení):
+    #       - Vytiskni: "Tohle je odpad: {vec}"
 
 
 
 # 5. Úplně na konci (mimo cyklus, bez odsazení vlevo):
-#    Vytiskni svůj nový seznam cenných věcí, ať vidíš, co sis odnesl domů.
-#    (Vypiš např. "Moje cenné věci: [seznam]")
+#    Vytiskni svůj nový seznam cenných věcí, ať vidíš, co sis odnesl domů.
+#    (Vypiš např. "Moje cenné věci: [seznam]")
